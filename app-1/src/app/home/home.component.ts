40441085eda6d849bb53cc4189957fc37b9d8688
a.nadirkhanloo <a.nadirkhanloo@asax.ir>
2024-03-04 18:44:09 +0330

    create home component and add route of home component into routing

---
+++ app-1/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone:true,
+  template: `
+  <h3>This is home component</h3>
+  `
+})
+export class HomeComponent {
+}
